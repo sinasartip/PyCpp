5c73b6c7723f94ef91c87f306ef5b2ce40adee6b
sinasartip <ssartipz@ryerson.ca>
2021-02-23 14:07:16 -0500

    example written out

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -6,5 +6,33 @@ namespace shapes{
     Rectangle::Rectangle(){}
 
     //Overload constructor
-    Rectangle::Rectangle
+    Rectangle::Rectangle(int x0, int y0, int x1, int y1){
+        this->x0 = x0;
+        this->y0 = y0;
+        this->x1 = x1;
+        this->y1 = y1;
+    }
+    
+    //Destructor
+    Rectangle::~Rectangle() {}
+
+    //Return the area of a rectangle
+    int Rectangle::getArea(){
+        return (this->x1 - this->x0) * (this->y1 - this->y0);
+    }
+
+    //Get the rectangle dimensions
+    //Place dimensions in pointer args
+    void Rectangle::getSize(int *width, int *height){
+        (*width) = x1 - x0;
+        (*height) = y1 - y0;
+    }
+
+    //Move rectangle by dx dy
+    void Rectangle::move(int dx, int dy){
+        this->x0 += dx;
+        this->x1 += dx;
+        this->y0 += dy;
+        this->y1 += dy;
+    }
 }
