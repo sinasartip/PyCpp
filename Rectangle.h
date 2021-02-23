22bb48aefc7fb7219e582858f8dbc6a432edf95d
sinasartip <ssartipz@ryerson.ca>
2021-02-23 17:36:14 -0500

    working example

--- Rectangle.h
+++ Rectangle.h
@@ -1,10 +1,10 @@
 #ifndef RECTANGLE_H
-#define RECTANLGE_H
+#define RECTANGLE_H
 
 namespace shapes {
-    class Rectangle{
+    class Rectangle {
         public:
-            int x0, y0, x1,y1;
+            int x0, y0, x1, y1;
             Rectangle();
             Rectangle(int x0, int y0, int x1, int y1);
             ~Rectangle();
